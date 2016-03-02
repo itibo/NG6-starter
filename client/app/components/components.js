1d0f1da12ba879c474f2d39da0a1b1555a557ec6
Stepan Kravtsevich <JohnDoe@itibo.com>
2016-03-02 18:23:54 +0300

    auth service

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,10 +1,13 @@
 import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
+import Auth from './auth/auth';
 
 let componentModule = angular.module('app.components', [
   Home.name,
-  About.name
+  About.name,
+
+  Auth.name
 ]);
 
 export default componentModule;
